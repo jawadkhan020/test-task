414ad9116660eb15b05f22a272c4897c4aa98019
jehad544 <m.jawadkhan020@gmail.com>
2022-11-30 01:45:24 +0500

    sad

--- app/Http/Controllers/API/VehicleController.php
+++ app/Http/Controllers/API/VehicleController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Category;
-use App\MOdels\Vehicle;
+use App\Models\Vehicle;
 use Validator;
 class VehicleController extends Controller
 {
